fbad6fabc3ca83ae40567c8018c411ac477451e0
zhengwei <zhengwei@viewhigh.com>
2018-12-11 16:28:07 +0800

    新增profileNames配置

---
+++ src/main/java/com/example/demoM/config/ProfileNames.java
@@ -0,0 +1,32 @@
+package com.example.demoM.config;
+
+public class ProfileNames {
+	
+	/**
+	 * 默认
+	 */
+	public static final String DEFAULT = "default";
+
+	/**
+	 * 本地开发测试环境
+	 */
+	public static final String DEV = "dev";
+
+	/**
+	 * Staging环境
+	 */
+	public static final String STAGING = "staging";
+	
+	/**
+	 * Demo环境(演示环境)
+	 */
+	public static final String DEMO = "demo";
+
+	/**
+	 * 生产环境
+	 */
+	public static final String PRODUCTION = "prod";
+
+	private ProfileNames() {
+	}
+}
--- src/main/java/com/example/demoM/config/WebConfig.java
+++ src/main/java/com/example/demoM/config/WebConfig.java
@@ -5,19 +5,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 import javax.servlet.DispatcherType;
-import javax.servlet.Filter;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
@@ -25,7 +19,6 @@ import org.springframework.validation.beanvalidation.MethodValidationPostProcess
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.multipart.support.MultipartFilter;
-import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -44,6 +37,7 @@ public class WebConfig extends WebMvcConfigurerAdapter {
     private Environment env;
 	
 	@Bean
+	@Profile({ProfileNames.DEFAULT, ProfileNames.DEV})
 	public FilterRegistrationBean corsFilterRegistrationBean() {
 		FilterRegistrationBean registrationBean = new FilterRegistrationBean();
 		registrationBean.setFilter(new CorsFilter());
@@ -57,46 +51,48 @@ public class WebConfig extends WebMvcConfigurerAdapter {
 	 }
 	 
 	 @Override
-		public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
-			super.configureMessageConverters(converters);
-			//对应无法直接返回String类型
-			converters.add(0, new MappingJackson2HttpMessageConverter(){
-	            @Override
-	            public ObjectMapper getObjectMapper() {
-	                super.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
-	                return super.getObjectMapper();
-	            }
-	            @Override
-	            public void setSupportedMediaTypes(List<MediaType> supportedMediaTypes) {
-	                List<MediaType> st = new ArrayList<MediaType>();
-	                MediaType text_plain = new MediaType(MediaType.TEXT_PLAIN, Charset.forName("UTF-8"));
-	                MediaType text_html = new MediaType(MediaType.TEXT_HTML, Charset.forName("UTF-8"));
-	                MediaType application_json = new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8"));
-	                MediaType application_xml = new MediaType(MediaType.APPLICATION_XML, Charset.forName("UTF-8"));
-	                MediaType application_xhtml_xml = new MediaType(MediaType.APPLICATION_XHTML_XML, Charset.forName("UTF-8"));
-	                MediaType x_www_form_urlencoded_utf8 = new MediaType(MediaType.APPLICATION_FORM_URLENCODED, Charset.forName("UTF-8"));
-	                st.add(text_html);
-	                st.add(text_plain);
-	                st.add(application_json);
-	                st.add(application_xml);
-	                st.add(application_xhtml_xml);
-	                st.add(MediaType.APPLICATION_JSON_UTF8);
-	                st.add(x_www_form_urlencoded_utf8);
-	                st.addAll(supportedMediaTypes);
-	                super.setSupportedMediaTypes(st);
-	            }
-	        });
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+		super.configureMessageConverters(converters);
+		// 对应无法直接返回String类型
+		converters.add(0, new MappingJackson2HttpMessageConverter() {
+			@Override
+			public ObjectMapper getObjectMapper() {
+				super.getObjectMapper().setSerializationInclusion(JsonInclude.Include.NON_NULL);
+				return super.getObjectMapper();
+			}
+
+			@Override
+			public void setSupportedMediaTypes(List<MediaType> supportedMediaTypes) {
+				List<MediaType> st = new ArrayList<MediaType>();
+				MediaType text_plain = new MediaType(MediaType.TEXT_PLAIN, Charset.forName("UTF-8"));
+				MediaType text_html = new MediaType(MediaType.TEXT_HTML, Charset.forName("UTF-8"));
+				MediaType application_json = new MediaType(MediaType.APPLICATION_JSON, Charset.forName("UTF-8"));
+				MediaType application_xml = new MediaType(MediaType.APPLICATION_XML, Charset.forName("UTF-8"));
+				MediaType application_xhtml_xml = new MediaType(MediaType.APPLICATION_XHTML_XML,
+						Charset.forName("UTF-8"));
+				MediaType x_www_form_urlencoded_utf8 = new MediaType(MediaType.APPLICATION_FORM_URLENCODED,
+						Charset.forName("UTF-8"));
+				st.add(text_html);
+				st.add(text_plain);
+				st.add(application_json);
+				st.add(application_xml);
+				st.add(application_xhtml_xml);
+				st.add(MediaType.APPLICATION_JSON_UTF8);
+				st.add(x_www_form_urlencoded_utf8);
+				st.addAll(supportedMediaTypes);
+				super.setSupportedMediaTypes(st);
+			}
+		});
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		// 只有非staging和非prod环境才启用swagger
+		if (env.acceptsProfiles(ProfileNames.DEFAULT, ProfileNames.DEV)) {
+			registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+			registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
 		}
-	 
-	 
-		@Override
-	    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-	        // 只有非staging和非prod环境才启用swagger
-//	        if (env.acceptsProfiles(ProfileNames.DEFAULT, ProfileNames.DEV)) {
-	            registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
-	            registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-//	        }
-	    }
+	}
 		
 //	@Bean
 //	public Filter tokenFilter() {
